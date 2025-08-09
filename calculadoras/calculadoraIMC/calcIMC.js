a2ff900407179ce91cad4a5d0cad23c5b4edce06
guizinnn12 <gui.andrade.corp@gmail.com>
2025-08-09 02:47:58 -0300

    calculadoras

---
+++ calculadoras/calculadoraIMC/calcIMC.js
@@ -0,0 +1,85 @@
+const form = document.querySelector('#formulario');
+
+form.addEventListener('reset', function() {
+    const limpar = document.querySelector('.result');
+    return limpar.innerHTML = '';
+});
+
+form.addEventListener('submit', function(event) {
+    event.preventDefault();
+    const inputPeso = document.querySelector('.peso');
+    const inputAltura = document.querySelector('.altura');
+    const Inputnome = document.querySelector('.nome');
+
+    const nome = Inputnome.value;
+    const peso = Number(inputPeso.value);
+    const altura = Number(inputAltura.value);
+
+    if (!peso) {
+        setResultado('Peso inválido', false);
+        return;
+    }
+
+    if (!altura) {
+        setResultado('Altura inválida', false);
+        return;
+    }
+
+    const imc = getImc(peso, altura); // função para calcular o IMC
+    const nivelImc = getNivelImc(imc); // função para o nivel do IMC
+
+    const msg = `${nome} seu IMC é ${imc} (${nivelImc}).`; // mensagem
+
+    setResultado(msg, true);
+});
+
+function getImc(peso, altura) { //Calculo do IMC
+    const imc = peso / altura ** 2;
+    return imc.toFixed(2); 
+}
+
+function getNivelImc (imc) { // nivel do IMC
+    const nivel = ['Abaixo do peso', 'Peso normal', 'Sobrepeso', 'Obesidade grau 1',
+        'Obesidade grau 2', 'Obesidade grau 3'];
+    
+    if (imc >= 39.9) return nivel[5]; 
+    if (imc >= 34.9) return nivel[4];   
+    if (imc >= 29.9) return nivel[3];    
+    if (imc >= 24.9) return nivel[2];    
+    if (imc >= 18.5) return nivel[1];   
+    if (imc < 18.5) return nivel[0];    
+}
+
+function criaP() { // função que cria um paragrafo
+    const p = document.createElement('p'); 
+    return p; 
+}
+
+function setResultado(msg, isValid) { // função que recebe uma mensagem e confirma se é valido
+    const resultado = document.querySelector('.result');
+    resultado.innerHTML = '';
+
+    const p = criaP(); 
+    
+    if (isValid) {
+        p.classList.add('paragrafo-resultado'); // cria uma class no paragráfo
+    } else {
+        p.classList.add('bad'); // cria uma class no paragráfo
+    }
+
+    p.innerHTML = msg;
+    resultado.appendChild(p);
+}
+
+/* ---- MENU HAMBÚRGUER ---- */
+const btnMenu = document.querySelector('.menu button');
+const menuOpcoes = document.querySelector('.menu-opcoes');
+
+btnMenu.addEventListener('click', (e) => {
+    e.preventDefault(); // evita comportamento de submit
+    if (menuOpcoes.style.display === '' || menuOpcoes.style.display === 'none') {
+        menuOpcoes.style.display = 'block';
+    } else {
+        menuOpcoes.style.display = 'none';
+    }
+});
---
+++ calculadoras/calculadoraTrad/script.js
@@ -0,0 +1,75 @@
+function Calculadora() {
+    this.display = document.querySelector('.display');
+
+    this.inicia = () => {
+        this.capturarCliques();
+        this.capturaEnter();
+    };
+
+    this.capturaEnter = () => {
+        document.addEventListener('keydown', (event) => {
+            if (event.key === "Enter") {
+                this.realizaConta();
+            }
+        });
+    };
+
+    this.teclasNumericas = () => {
+        document.addEventListener('keydown', (e) => {
+            const teclasPermitidas = "0123456789+-*/.()";
+            if(teclasPermitidas.includes(e.key)) {
+                this.display.value += e.key;
+            } 
+        });
+    };
+
+    this.capturarCliques = () => {
+        document.addEventListener('click', event => {
+            const el = event.target;
+            if (el.classList.contains('btn-num')) this.addNumDisplay(el);
+            if (el.classList.contains('btn-clear')) this.clear();
+            if (el.classList.contains('btn-del')) this.del();
+            if (el.classList.contains('btn-igual')) this.realizaConta();
+        });
+    };
+
+    this.realizaConta = () => {
+        try {
+            const conta = eval(this.display.value);
+
+            if (!conta) {
+                alert('Conta inválida!');
+                return;
+            }
+
+        this.display.value = conta;
+        } catch(e) {
+            alert('Conta inválida!');
+            return;
+        }
+    };
+
+    this.addNumDisplay = el => {
+        this.display.value += el.innerText;
+        this.display.focus();
+    };
+
+    this.clear = () => this.display.value = '';
+    this.del = () => this.display.value = this.display.value.slice(0, -1);
+}
+
+/* ---- MENU HAMBÚRGUER ---- */
+const btnMenu = document.querySelector('.menu button');
+const menuOpcoes = document.querySelector('.menu-opcoes');
+
+btnMenu.addEventListener('click', (e) => {
+    e.preventDefault(); // evita comportamento de submit
+    if (menuOpcoes.style.display === '' || menuOpcoes.style.display === 'none') {
+        menuOpcoes.style.display = 'block';
+    } else {
+        menuOpcoes.style.display = 'none';
+    }
+});
+
+const calculadora = new Calculadora();
+calculadora.inicia();
