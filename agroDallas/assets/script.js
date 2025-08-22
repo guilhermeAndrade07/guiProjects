9b48c6685dff9a71fec164a4438c8ed248333ffd
guizinnn12 <gui.andrade.corp@gmail.com>
2025-08-22 10:55:47 -0300

    Arrumando as seleções

--- agroDallas/assets/script.js
+++ agroDallas/assets/script.js
@@ -1,11 +1,39 @@
-const btnCalcular = document.querySelector('.botao button');
-const resultado = document.querySelector('.resultado');
+const cultivo = document.querySelector('.selectCultivo');
+const produto = document.querySelector('.selectProduto');
+const litros = document.querySelector('#litros');
+const calcular = document.querySelector('.btnCalcular');
+const resultado = document.querySelector('.resultadoTxt');
 
-btnCalcular.addEventListener('click', (e) => {
-    e.preventDefault();
-    if (resultado.style.display === '' || resultado.style.display === 'none') {
-        resultado.style.display = 'block';
-    } else {
-        resultado.style.display === 'none';
+calcular.addEventListener('click', function () {
+    const cultivoSelecionado = cultivo.value;
+    const produtoSelecionado = produto.value;
+    const qtdLitros = parseFloat(litros.value);
+
+    if (!cultivoSelecionado || !produtoSelecionado || isNaN(qtdLitros)) {
+    resultado.textContent = "⚠️ Preencha todas as informações!";
+    return;
     }
+
+    let dosagemPorLitro = 0;
+
+    if (cultivoSelecionado === 'hortalicas' && produtoSelecionado === 'npk') {
+        dosagemPorLitro = 6;
+    }
+
+    if (dosagemPorLitro  === 0) {
+    resultado.textContent = "❌ Não existe dosagem cadastrada para essa combinação!";
+    return;
+    }
+
+    const dosagemTotal = dosagemPorLitro * qtdLitros;
+
+    resultado.textContent = `✅ Dosagem recomendada → ${dosagemTotal.toFixed(1)} ml de ${produtoSelecionado.toUpperCase()} para ${qtdLitros}L de água`;
 });
+
+
+
+
+
+
+
+
