fcc211af47af429963915dd96a80032d2b9f7a04
guizinnn12 <gui.andrade.corp@gmail.com>
2025-08-22 13:24:04 -0300

    parte final do site

--- agroDallas/assets/script.js
+++ agroDallas/assets/script.js
@@ -15,19 +15,116 @@ calcular.addEventListener('click', function () {
     }
 
     let dosagemPorLitro = 0;
+    let hectare = '';
 
+    // hortalicas
     if (cultivoSelecionado === 'hortalicas' && produtoSelecionado === 'npk') {
         dosagemPorLitro = 6;
+        hectare = '1 Litros do produto por hectare.';
+    }
+    if (cultivoSelecionado === 'hortalicas' && produtoSelecionado === 'nitro30') {
+        dosagemPorLitro = 6;
+        hectare = '1 Litros do produto por hectare.';
+    }
+    if (cultivoSelecionado === 'hortalicas' && produtoSelecionado === 'phos30') {
+        dosagemPorLitro = 6;
+        hectare = '1 Litros do produto por hectare.';
+    }
+    if (cultivoSelecionado === 'hortalicas' && produtoSelecionado === 'turfa') {
+        dosagemPorLitro = 10;
+        hectare = '3 - 5 Litros do produto por hectare.';
+    }
+    if (cultivoSelecionado === 'hortalicas' && produtoSelecionado === 'calMag+') {
+        dosagemPorLitro = 10;
+        hectare = '3 - 5 Litros do produto por hectare.';
+    }
+    if (cultivoSelecionado === 'hortalicas' && produtoSelecionado === 'trikronix') {
+        dosagemPorLitro = 4;
     }
 
-    if (dosagemPorLitro  === 0) {
+    // Grãos
+    if (cultivoSelecionado === 'graos' && produtoSelecionado === 'npk') {
+        dosagemPorLitro = 10;
+        hectare = '2 Litros do produto por hectare.';
+    }
+    if (cultivoSelecionado === 'graos' && produtoSelecionado === 'nitro30') {
+        dosagemPorLitro = 10;
+        hectare = '2 Litros do produto por hectare.';
+    }
+    if (cultivoSelecionado === 'graos' && produtoSelecionado === 'phos30') {
+        dosagemPorLitro = 10;
+        hectare = '2 Litros do produto por hectare.';
+    }
+    if (cultivoSelecionado === 'graos' && produtoSelecionado === 'turfa') {
+        dosagemPorLitro = 10;
+        hectare = '4 - 5 Litros do produto por hectare.';
+    }
+    if (cultivoSelecionado === 'graos' && produtoSelecionado === 'calMag+') {
+        dosagemPorLitro = 10;
+        hectare = '4 - 5 Litros do produto por hectare.';
+    }
+    if (cultivoSelecionado === 'graos' && produtoSelecionado === 'trikronix') {
+        dosagemPorLitro = 4;
+    }
+
+    // Frutifera
+    if (cultivoSelecionado === 'frutifera' && produtoSelecionado === 'npk') {
+        dosagemPorLitro = 10;
+        hectare = '1 Litros do produto por hectare.';
+    }
+    if (cultivoSelecionado === 'frutifera' && produtoSelecionado === 'nitro30') {
+        dosagemPorLitro = 10;
+        hectare = '1 Litros do produto por hectare.';
+    }
+    if (cultivoSelecionado === 'frutifera' && produtoSelecionado === 'phos30') {
+        dosagemPorLitro = 10;
+        hectare = '1 Litros do produto por hectare.';
+    }
+    if (cultivoSelecionado === 'frutifera' && produtoSelecionado === 'turfa') {
+        dosagemPorLitro = 10;
+        hectare = '4 - 5 Litros do produto por hectare.';
+    }
+    if (cultivoSelecionado === 'frutifera' && produtoSelecionado === 'calMag+') {
+        dosagemPorLitro = 10;
+        hectare = '4 - 5 Litros do produto por hectare.';
+    }
+    if (cultivoSelecionado === 'frutifera' && produtoSelecionado === 'trikronix') {
+        dosagemPorLitro = 4;
+    }
+
+    // Pastagem/Gramado
+    if (cultivoSelecionado === 'pastagem' && produtoSelecionado === 'npk') {
+        dosagemPorLitro = 10;
+        hectare = '2 - 4 Litros do produto por hectare.';
+    }
+    if (cultivoSelecionado === 'pastagem' && produtoSelecionado === 'nitro30') {
+        dosagemPorLitro = 10;
+        hectare = '2 - 4 Litros do produto por hectare.';
+    }
+    if (cultivoSelecionado === 'pastagem' && produtoSelecionado === 'phos30') {
+        dosagemPorLitro = 10;
+        hectare = '2 Litros do produto por hectare.';
+    }
+    if (cultivoSelecionado === 'pastagem' && produtoSelecionado === 'turfa') {
+        dosagemPorLitro = 10;
+        hectare = '5 Litros do produto por hectare.';
+    }
+    if (cultivoSelecionado === 'pastagem' && produtoSelecionado === 'calMag+') {
+        dosagemPorLitro = 10;
+        hectare = '5 Litros do produto por hectare.';
+    }
+    if (cultivoSelecionado === 'pastagem' && produtoSelecionado === 'trikronix') {
+        dosagemPorLitro = 4;
+    }
+
+    if (qtdLitros  === 0) {
     resultado.textContent = "❌ Não existe dosagem cadastrada para essa combinação!";
     return;
     }
 
     const dosagemTotal = dosagemPorLitro * qtdLitros;
 
-    resultado.textContent = `✅ Dosagem recomendada → ${dosagemTotal.toFixed(1)} ml de ${produtoSelecionado.toUpperCase()} para ${qtdLitros}L de água`;
+    resultado.textContent = `${dosagemTotal.toFixed(0)}ml de ${produtoSelecionado.toUpperCase()} para ${qtdLitros}L de água` +  ' ' + `✅Dosagem recomendada: ${hectare}`;
 });
 
 
